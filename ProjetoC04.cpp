cee4431a2daf5bbe283b60dbed9aa506fd789893
Pedro Gabriel da Silva <p3dr0.tr@gmail.com>
2026-08-19 12:23:03 -0300

    fiz uma mudançinhas

--- ProjetoC04.cpp
+++ ProjetoC04.cpp
@@ -3,7 +3,7 @@
 using namespace std; 
 
   //Pedro Gabriel da Silva 739
-  //Jo�o Pedro Ramos Garcia 820
+  //João Pedro Ramos Garcia 820
   //Mariana Novais Portugal 746
   //Emily Horrana Lima Rodrigues Armando 787
   //Vinicius Oliveira Braga 743
@@ -18,42 +18,42 @@ struct item
 
 int insert()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }  
 int cadastrar()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int buscar()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int verificar()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int listarA()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int listarR()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int contar()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 int remover()
 {
-  cout << "Funcionalidade em constru��o" << endl;
+  cout << "Funcionalidade em construção" << endl;
   return 0;
 }
 
@@ -61,16 +61,20 @@ int main()
 {  
   string item[100];
   int opcao;
-
-  cout << "1. Inserir item" <<endl;
-  cout << "2. Cadastrar similaridade entre itens." <<endl;
-  cout << "3. Buscar itens similares." << endl;
-  cout << "4. Verificar a exist�ncia de um item. " <<endl;
-  cout << "5. Listar itens (ordem alfab�tica de nome)." << endl;
-  cout << "6. Listar itens (ordem decrescente de raridade)." <<endl;
-  cout << "7. Contar itens com mesma propriedade m�gica. " << endl;
-  cout << "8. Remover itens menos raros. " << endl;
-
+  
+	cout << "=================================================" << endl;
+	cout << "                    M E N U                      " << endl;
+	cout << "=================================================" << endl;
+	cout << "1. Inserir item                                 " <<endl;
+	cout << "2. Cadastrar similaridade entre itens.          " <<endl; 
+	cout << "3. Buscar itens similares.                      " << endl;
+	cout << "4. Verificar a existência de um item.           " <<endl;
+	cout << "5. Listar itens (ordem alfabética de nome).     " << endl;
+	cout << "6. Listar itens (ordem decrescente de raridade)." <<endl;
+	cout << "7. Contar itens com mesma propriedade mágica.   " << endl;
+	cout << "8. Remover itens menos raros.                   " << endl;
+	cout << "------------------------------------------------" << endl;
+ 
   cin >> opcao;
 
   switch(opcao)
